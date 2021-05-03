09aeb9c73cfae11e54be3581362f286d026469be
Juan Pablo <juanpablogast@gmail.com>
2021-05-03 17:19:52 -0400

    First

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  resources :users, only: [:new, :create, :show]
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
